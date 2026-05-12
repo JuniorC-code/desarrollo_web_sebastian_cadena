2b2334f4c65425eee9ceefca8f38b44baba820cd
JuniorC-code <sebastiancadena2013@gmail.com>
2026-05-12 19:21:17 -0400

    Feat: Creacion de DB, junto con app de flask.

---
+++ flask_app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+
+UPLOAD_FOLDER = 'static/uploads'
+
+app = Flask(__name__)
+
+
+app.secret_key = "s3cr3t_k3y"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
