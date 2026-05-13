8cba11a263b3484b8a84b0ee66ead5b32296c61a
JuniorC-code <sebastiancadena2013@gmail.com>
2026-05-12 20:41:57 -0400

    Feat: Se añade db.py con su ORM, junto con entrelazar index.html a js y css.

---
+++ flask_app/database/db.py
@@ -0,0 +1,234 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Enum, Text
+
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+# Configuración de la base de datos
+DB_NAME = "tarea2"
+DB_USERNAME = "root"
+DB_PASSWORD = "admin"
+DB_HOST = "localhost"
+DB_PORT = 3306
+
+DATABASE_URL = f"mysql+pymysql://"f"{DB_USERNAME}:{DB_PASSWORD}"f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+# modelos de la base de datos
+
+class Region(Base):
+    __tablename__ = "region"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(200), nullable=False)
+
+    #Relaciones
+    comunas = relationship( "Comuna", back_populates="region", cascade="all, delete")
+
+
+class Comuna(Base):
+    __tablename__ = "comuna"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(200), nullable=False)
+    region_id = Column( Integer, ForeignKey("region.id"), nullable=False)
+
+    # relaciones
+    region = relationship("Region",back_populates="comunas")
+    miembros = relationship("Miembro",back_populates="comuna",cascade="all, delete")
+
+
+class Miembro(Base):
+    __tablename__ = "miembro"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(255), nullable=False)
+    email = Column(String(80), nullable=False)
+    telefono = Column(String(15), nullable=False)
+    fecha_registro = Column(DateTime, nullable=False)
+    comuna_id = Column( Integer, ForeignKey("comuna.id"), nullable=False )
+
+    # relaciones
+    comuna = relationship( "Comuna", back_populates="miembros" )
+    actividades = relationship("Actividad",back_populates="miembro",cascade="all, delete")
+
+
+class Actividad(Base):
+    __tablename__ = "actividad"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    miembro_id = Column( Integer, ForeignKey("miembro.id"), nullable=False )
+    dia = Column(
+        Enum(
+            "lunes",
+            "martes",
+            "miércoles",
+            "jueves",
+            "viernes",
+            "sábado",
+            "domingo"
+        ),
+        nullable=False
+    )
+    hora_inicio = Column(String(5), nullable=False)
+    duracion = Column(String(5), nullable=False)
+    tipo = Column(
+        Enum(
+            "arte",
+            "deporte",
+            "tecnología",
+            "social",
+            "recreación",
+            "otra"
+        ),
+        nullable=False
+    )
+    nombre = Column(String(45), nullable=False)
+    descripcion = Column(Text, nullable=True)
+
+    # relaciones
+    miembro = relationship( "Miembro", back_populates="actividades" )
+    fotos = relationship( "Foto", back_populates="actividad", cascade="all, delete" )
+
+
+class Foto(Base):
+    __tablename__ = "foto"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ruta_archivo = Column(String(300), nullable=False)
+    nombre_archivo = Column(String(300), nullable=False)
+    actividad_id = Column( Integer, ForeignKey("actividad.id"), nullable=False )
+
+    # relaciones
+    actividad = relationship( "Actividad", back_populates="fotos" )
+
+# =====================================================
+# Funciones para trabajar con la base de datos
+# =====================================================
+
+
+# REGIONES
+
+def get_regiones():
+    session = SessionLocal()
+    regiones = session.query(Region).all()
+    session.close()
+    return regiones
+
+def get_region_by_id(region_id):
+    session = SessionLocal()
+    region = (session.query(Region).filter_by(id=region_id).first())
+    session.close()
+    return region
+
+
+# COMUNAS
+
+def get_comunas():
+    session = SessionLocal()
+    comunas = session.query(Comuna).all()
+    session.close()
+    return comunas
+
+def get_comunas_by_region(region_id):
+    session = SessionLocal()
+    comunas = (session.query(Comuna).filter_by(region_id=region_id).all())
+    session.close()
+    return comunas
+
+
+def get_comuna_by_id(comuna_id):
+    session = SessionLocal()
+    comuna = (session.query(Comuna).filter_by(id=comuna_id).first())
+    session.close()
+    return comuna
+
+
+# MIEMBROS
+
+def create_miembro( nombre, email, telefono, fecha_registro, comuna_id):
+
+    session = SessionLocal()
+
+    nuevo_miembro = Miembro(
+        nombre=nombre,
+        email=email,
+        telefono=telefono,
+        fecha_registro=fecha_registro,
+        comuna_id=comuna_id
+    )
+
+    session.add(nuevo_miembro)
+    session.commit()
+    miembro_id = nuevo_miembro.id
+    session.close()
+    return miembro_id
+
+
+def get_miembro_by_id(miembro_id):
+    session = SessionLocal()
+    miembro = (session.query(Miembro).filter_by(id=miembro_id).first())
+    session.close()
+    return miembro
+
+
+# ACTIVIDADES
+
+def create_actividad( miembro_id, dia, hora_inicio, duracion, tipo, nombre, descripcion):
+
+    session = SessionLocal()
+
+    nueva_actividad = Actividad(
+        miembro_id=miembro_id,
+        dia=dia,
+        hora_inicio=hora_inicio,
+        duracion=duracion,
+        tipo=tipo,
+        nombre=nombre,
+        descripcion=descripcion
+    )
+
+    session.add(nueva_actividad)
+    session.commit()
+    actividad_id = nueva_actividad.id
+    session.close()
+    return actividad_id
+
+
+def get_actividades():
+    session = SessionLocal()
+    actividades = session.query(Actividad).all()
+    session.close()
+    return actividades
+
+
+def get_actividad_by_id(actividad_id):
+    session = SessionLocal()
+    actividad = (session.query(Actividad).filter_by(id=actividad_id).first())
+    session.close()
+    return actividad
+
+
+# FOTOS
+
+def create_foto(ruta_archivo,nombre_archivo,actividad_id):
+    session = SessionLocal()
+
+    nueva_foto = Foto(
+        ruta_archivo=ruta_archivo,
+        nombre_archivo=nombre_archivo,
+        actividad_id=actividad_id
+    )
+
+    session.add(nueva_foto)
+    session.commit()
+    session.close()
+
+
+def get_fotos_by_actividad(actividad_id):
+    session = SessionLocal()
+    fotos = (session.query(Foto).filter_by(actividad_id=actividad_id).all())
+    session.close()
+    return fotos
